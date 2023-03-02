037adac1288ce3ba73b8b9cbbb460cbd49fd5f14
SKupisz <szym-ku@wp.pl>
2023-03-02 17:32:39 +0100

    Added dummy data of teams to prepare data rendering for the future

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,32 @@
 import { RankingWrapper } from "@/styled/ranking"
 import RankingRowComponent from "components/RankingRowComponent"
 
+type TeamType = {
+  name: string;
+  money: number;
+  change: number;
+}
+
 export default function Home() {
+
+  const teams:TeamType[] = [
+    {
+      name: "Team 1",
+      money: 10000,
+      change: 0.1
+    },
+    {
+      name: "Team 2",
+      money: 8000,
+      change: -0.02
+    },
+    {
+      name: "Team 3",
+      money: 10100,
+      change: 0.01
+    },
+  ]
+
   return (
     <>
       <RankingWrapper>
@@ -12,12 +37,15 @@ export default function Home() {
           column3="Stan"
           column4="Zmiana"
           />
-        <RankingRowComponent 
-          column1="1"
-          column2="test"
-          column3="10.000$"
-          column4="+10%"
-          />
+        {
+          teams.map((elem:TeamType, index:number) => 
+            <RankingRowComponent 
+              column1={(index+1).toString()}
+              column2={elem.name}
+              column3={elem.money.toString()+"$"}
+              column4={(elem.change*100).toString()+"%"}
+              />)
+        }
       </RankingWrapper>
     </>
   )
